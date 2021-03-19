f37074fd0d4333fa82c92735f2572bd11e3d94dc
Shayon G <gshayon122@hotmail.com>
2021-03-19 20:55:35 +0530

    webpages started

--- index.js
+++ index.js
@@ -2,13 +2,50 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('view engine',path.join(__dirname,'/public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views'));
 
+const farmer = [
+    {
+        name: "Akash",
+        pw: 123,
+        id: 0001,
+        ph: 1234567890,
+        aadhar: 000011112222,
+        kisancard: 8564,
+        location: "Arunachal",
+        pref_comm: "rice" 
+    }
+]
+
+const buyer = [
+    {
+        name: "Arjun",
+        pw: 123,
+        ph: 1234523456,
+        aadhar: 333344445555,
+        pref_comm: "rice",
+        addr: "Arunachal"
+    }
+]
+
+
+
 app.get('/', (req,res) => {
-    res.render('Home');
+    res.render('home');
+});
+
+app.get('/buyerlogin', (req,res) => {
+    res.render('buyerlogin');
+});
+
+app.get('/farmerlogin', (req,res) => {
+    res.render('farmerlogin');
 });
 
 app.listen(8000, ()=>{
